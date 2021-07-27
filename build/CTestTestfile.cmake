34a3f06623b1b033077d8b9913a5fff1e6e9e4ab
bjliu <bjliu@lscm.hk>
2021-07-27 17:05:12 +0800

    align param file names

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lscm/InvestHKRobot_ROS/src
-# Build directory: /home/lscm/InvestHKRobot_ROS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("robot_navigation")
