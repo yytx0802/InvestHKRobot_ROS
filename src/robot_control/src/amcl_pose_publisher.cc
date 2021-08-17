8f3eb358b911d032fa22c0bd5f6af8d17e480eed
yytx0802 <hellohit2011@gmail.com>
2021-08-17 10:56:39 +0800

    feat: add new node to publish amcl tf pose

---
+++ src/robot_control/src/amcl_pose_publisher.cc
@@ -0,0 +1,49 @@
+#include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "amcl_pose_publisher");
+
+  ros::NodeHandle node;
+
+  ros::Publisher amcl_pose_publisher =
+    node.advertise<geometry_msgs::PoseWithCovarianceStamped>("amcl_tf_pose", 10);
+
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+
+  ros::Rate rate(10.0);
+  while (node.ok()){
+    geometry_msgs::TransformStamped transformStamped;
+    try{
+      transformStamped = tfBuffer.lookupTransform("odom", "base_link",
+                               ros::Time(0));
+    }
+    catch (tf2::TransformException &ex) {
+      ROS_WARN("%s",ex.what());
+      ros::Duration(1.0).sleep();
+      continue;
+    }
+
+    geometry_msgs::PoseWithCovarianceStamped amcl_pose;
+    amcl_pose.pose.pose.position.x = transformStamped.transform.translation.x;
+    amcl_pose.pose.pose.position.y = transformStamped.transform.translation.y;
+    amcl_pose.pose.pose.position.z = transformStamped.transform.translation.z;
+    //tf::Quaternion q(0, 0, pose.pose.pose.orientation.z, pose.pose.pose.orientation.w);
+    //tf::Matrix3x3 m(q);
+    //double roll, pitch, yaw;
+    //m.getRPY(roll, pitch, yaw);
+    amcl_pose.pose.pose.orientation.x = transformStamped.transform.rotation.x;
+    amcl_pose.pose.pose.orientation.y = transformStamped.transform.rotation.y;
+    amcl_pose.pose.pose.orientation.z = transformStamped.transform.rotation.z;
+    amcl_pose.pose.pose.orientation.w = transformStamped.transform.rotation.w;
+
+    amcl_pose_publisher.publish(amcl_pose);
+
+    rate.sleep();
+  }
+  return 0;
+};
+
